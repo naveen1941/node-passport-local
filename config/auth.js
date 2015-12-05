9e0ced5ed39a567f8b590cceee6845ccdccf6f5e
naveen1941 <chittipolu@gmail.com>
2015-12-05 15:59:51 +0530

    Init commit passport-local-strategy

---
+++ config/auth.js
@@ -0,0 +1,20 @@
+var ids={
+	facebook:{
+		clientId:'518550501660359',
+		clientSecret:'5d539c61c21b82de8d2982fb9fce989a',
+		callbackURL:'http://127.0.0.1:3000/auth/facebook/callback/'
+	},
+	google:{
+		clientId:'689182502014-uuk3033ot3r5v6qf3vr629vtl3gdoo7a.apps.googleusercontent.com',
+		clientSecret:'pCPlfwbsQSdeHoPwiMeKBQOw',
+		callbackURL:'http://127.0.0.1:3000/auth/google/callback/'
+	}
+};
+module.exports = ids;
+ 
+//set FACEBOOK_CLIENT_ID=518550501660359
+//set FACEBOOK_KEY=518550501660359
+ 
+//set FACEBOOK_SECRET=5d539c61c21b82de8d2982fb9fce989a
+//set FACEBOOK_CLIENT_SECRET=5d539c61c21b82de8d2982fb9fce989a
+//set FACEBOOK_CALLBACK_URL=http://127.0.0.1:3000/auth/facebook/callback/
